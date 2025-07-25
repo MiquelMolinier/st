5ca5ea8bc5f2d63f99f799e4f3c86270f834ad02
miquelmolinier <miquelmolinier@gmail.com>
2025-07-25 15:44:04 -0500

    alpha, changealpha

--- config.h
+++ config.h
@@ -93,6 +93,12 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.9;
+
+/* Background opacity */
+float alpha_def;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -188,19 +194,22 @@ static MouseShortcut mshortcuts[] = {
 #define TERMMOD (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
-	/* mask                 keysym          function        argument */
-	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
-	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
-	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
-	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	/* mask                 keysym              function        argument */
+	{ XK_ANY_MOD,           XK_Break,           sendbreak,      {.i =  0} },
+	{ ControlMask,          XK_Print,           toggleprinter,  {.i =  0} },
+	{ ShiftMask,            XK_Print,           printscreen,    {.i =  0} },
+	{ XK_ANY_MOD,           XK_Print,           printsel,       {.i =  0} },
+	{ TERMMOD,              XK_Prior,           zoom,           {.f = +1} },
+	{ TERMMOD,              XK_Next,            zoom,           {.f = -1} },
+	{ TERMMOD,              XK_Home,            zoomreset,      {.f =  0} },
+	{ TERMMOD,              XK_C,               clipcopy,       {.i =  0} },
+	{ TERMMOD,              XK_V,               clippaste,      {.i =  0} },
+	{ TERMMOD,              XK_Y,               selpaste,       {.i =  0} },
+	{ ShiftMask,            XK_Insert,          selpaste,       {.i =  0} },
+	{ TERMMOD,              XK_Num_Lock,        numlock,        {.i =  0} },
+	{ MODKEY,               XK_bracketleft,     chgalpha,       {.f = -1} }, /* Decrease opacity */
+	{ MODKEY,               XK_bracketright,    chgalpha,       {.f = +1} }, /* Increase opacity */
+	{ MODKEY|ShiftMask,     XK_bar,             chgalpha,       {.f =  0} }, /* Reset opacity */
 };
 
 /*
